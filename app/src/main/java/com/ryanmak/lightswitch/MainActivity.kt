9cde0623eebd1933fc68817a8c7bdab1148c45fd
Ryan Mak <ryan.mak1996@gmail.com>
2022-05-27 00:20:34 -0400

    feature: added ability to save settings

--- app/src/main/java/com/ryanmak/lightswitch/MainActivity.kt
+++ app/src/main/java/com/ryanmak/lightswitch/MainActivity.kt
@@ -2,53 +2,64 @@ package com.ryanmak.lightswitch
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.ryanmak.lightswitch.databinding.ActivityMainBinding
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-    companion object {
-        val DIM_ENABLED = "dim_enabled"
-        val DIM_INTENSITY = "dim_intensity"
-        val SCREEN_ON_ENABLED = "screen_on_enabled"
-    }
-
     private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
         setupListeners()
+        setupFlows()
     }
 
     private fun setupListeners() {
         binding.dimScreenRadioGroup.setOnCheckedChangeListener { _, id ->
-            when(id) {
-                R.id.onButtonDim -> {
+            viewModel.setDimEnabled(id == R.id.onButtonDim)
+        }
 
-                }
+        binding.slider.addOnChangeListener { _, value, _ ->
+            viewModel.setDimIntensity(value)
+        }
 
-                R.id.offButtonDim -> {
+        binding.screenOnRadioGroup.setOnCheckedChangeListener { _, id ->
+            viewModel.setScreenOnEnabled(id == R.id.onButtonScreenOn)
+        }
+    }
 
-                }
+    private fun setupFlows() {
+        lifecycleScope.launch {
+            viewModel.dimEnabledFlow.collect { enabled ->
+                binding.dimScreenRadioGroup.check(
+                    if (enabled) R.id.onButtonDim
+                    else R.id.offButtonDim
+                )
             }
         }
 
-        binding.screenOnRadioGroup.setOnCheckedChangeListener { _ , id ->
-            when(id) {
-                R.id.onButtonScreenOn -> {
-
-                }
-
-                R.id.offButtonScreenOn -> {
-
-                }
+        lifecycleScope.launch {
+            viewModel.dimIntensityFlow.collect { value ->
+                binding.slider.value = value
             }
         }
 
-        binding.slider.addOnChangeListener { slider, value, fromUser ->
-
+        lifecycleScope.launch {
+            viewModel.screenOnEnabledFlow.collect { enabled ->
+                binding.screenOnRadioGroup.check(
+                    if (enabled) R.id.onButtonScreenOn
+                    else R.id.offButtonScreenOn
+                )
+            }
         }
     }
 }
--- app/src/main/java/com/ryanmak/lightswitch/MainActivityViewModel.kt
+++ app/src/main/java/com/ryanmak/lightswitch/MainActivityViewModel.kt
@@ -1,6 +1,45 @@
 package com.ryanmak.lightswitch
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.lifecycle.AndroidViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 
-class MainActivityViewModel : ViewModel() {
+class MainActivityViewModel(app: Application) : AndroidViewModel(app) {
+
+    companion object {
+        private const val KEY_SETTINGS = "settings"
+        private const val DIM_ENABLED = "dim_enabled"
+        private const val DIM_INTENSITY = "dim_intensity"
+        private const val SCREEN_ON_ENABLED = "screen_on_enabled"
+    }
+
+    private var sharedPrefs: SharedPreferences
+
+    val dimEnabledFlow = MutableStateFlow(false)
+    val dimIntensityFlow = MutableStateFlow(0f)
+    val screenOnEnabledFlow = MutableStateFlow(false)
+
+    init {
+        sharedPrefs = app.getSharedPreferences(KEY_SETTINGS, Context.MODE_PRIVATE)
+        dimEnabledFlow.value = sharedPrefs.getBoolean(DIM_ENABLED, false)
+        dimIntensityFlow.value = sharedPrefs.getFloat(DIM_INTENSITY, 0f)
+        screenOnEnabledFlow.value = sharedPrefs.getBoolean(SCREEN_ON_ENABLED, false)
+    }
+
+    fun setDimEnabled(value: Boolean) {
+        dimEnabledFlow.value = value
+        sharedPrefs.edit().putBoolean(DIM_ENABLED, value).apply()
+    }
+
+    fun setDimIntensity(value: Float) {
+        dimIntensityFlow.value = value
+        sharedPrefs.edit().putFloat(DIM_INTENSITY, value).apply()
+    }
+
+    fun setScreenOnEnabled(value: Boolean) {
+        screenOnEnabledFlow.value = value
+        sharedPrefs.edit().putBoolean(SCREEN_ON_ENABLED, value).apply()
+    }
 }
